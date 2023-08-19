e1ba9980f729afd8a1d8c360a86cfc58c6e7fa8a
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-08-19 19:06:06 +0100

    update

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let Inputs = document.querySelector('#input');
 let birthDay = document.querySelector('#Day');
 let birthMonth = document.querySelector('#Month');
 let birthYear = document.querySelector('#Year');
+
+let todaysDate = new Date()
