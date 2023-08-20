1fe0082b5f674c6dd848724e222fc9f190e5fafb
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-08-20 20:55:26 +0100

    adding javascript

--- script.js
+++ script.js
@@ -22,5 +22,46 @@ let bday;
 let bmonth;
 let byear;
 
+let button = document.querySelector('#button')
 
 button.addEventListener('click',check);
+
+function check(){
+if(birthDay.value===''||birthMonth.value===''||birthYear.value===''||birthDay.value>31||birthMonth.value>12||birthYear.value>2023){
+    if(birthDay.value===''){
+      showError(birthDay,'This field is required');
+    } 
+    else{
+       anotherError(birthDay);
+    }
+
+    if(birthMonth.value==='' ){
+       showError(birthMonth ,'This field is required');
+    }
+    else{
+      anotherError(birthMonth);
+    }
+ 
+    if(birthYear.value===''){
+      showError(birthYear,'This field is required');
+    }
+    else{
+      anotherError(birthYear);
+    }
+
+    if(birthDay.value>31){
+      isValid(birthDay ,'Must be a valid day');
+    }
+    if(birthMonth.value>12){
+      isValid(birthMonth ,'Must be a valid month');
+    }
+ 
+    if(birthYear.value>2023){
+       isValid(birthYear ,'Must be in the past');
+    }
+}
+else{
+     success(birthDay,birthMonth,birthYear);
+    ageCalc();
+}
+}
