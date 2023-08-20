8d11252e52bd4200fcf951777f6dc5ad42fe693c
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-08-20 20:27:17 +0100

    adding javascript

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let Inputs = document.querySelector('#input');
-let birthDay = document.querySelector('#Day');
-let birthMonth = document.querySelector('#Month');
-let birthYear = document.querySelector('#Year');
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+//my birth date
+let birthDay = document.getElementById('Day');
+let birthMonth = document.getElementById('Month');
+let birthYear = document.getElementById('Year');
+let Inputs = document.getElementById('input');
 
-let todaysDate = new Date()
+let todayDate = new Date();
